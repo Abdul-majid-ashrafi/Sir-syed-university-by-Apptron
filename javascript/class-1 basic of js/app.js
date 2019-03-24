2b13d33767ac95af035112faf2f7dd003f444d84
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2019-03-24 03:07:47 -0700

    learn basic variables alert if condition and prompt

---
+++ javascript/class-1 basic of js/app.js
@@ -0,0 +1,94 @@
+// alert("HELLO        ");
+// alert("HELLO      World");
+// alert(data);
+// alert(1233);
+// alert(1233.64);
+
+
+// console.log("HEELO")
+// console.error("HEELO")
+// console.warn("HEELO")
+
+
+
+
+
+
+
+// // varible declaration
+// var myName;
+
+// // varible initilazation
+// myName = "Majid";
+
+// console.log(myName)
+
+
+
+
+// var myName = "Majid";
+// var age = "27";
+// var address = "karachi";
+// console.log(address)
+
+// address = "Lahore";
+// console.log(address)
+
+// address = "Islamabad";
+// console.log(address)
+
+
+// var number = 123;
+// console.log(typeof number, number - 7)
+// number = "123";
+// console.log(typeof number, number - 7)
+// number = "Majid "
+// console.log(typeof number, number - 7)
+
+
+// // concatination
+// var userName = "Majid";
+// var greeting = "Hello";
+// var msg = greeting + userName
+// console.log(greeting + " " + userName + "!")
+
+
+
+// var 2;            //illegal
+// var 2things;      //illegal
+// var .things;      //illegal
+// var %things;      //illegal
+// var -things;      //illegal
+// var @things;      //illegal
+// var #things;      //illegal
+// var _things;      //legal
+// var $things;      //legal
+// var th4ings2;      //legal
+// var twoThings;    //legal
+// var TwoThings;    //legal
+// var Two2Things;   //legal
+
+
+// increment/decrement
+// // var num = 1
+// var count = 19;
+// // count = count + 1;
+// ++count
+// console.log(count)
+// // --count
+// // console.log(count)
+
+
+// Prompt
+// var getUserName;
+// getUserName = prompt("What's you name", "Majid Ashraf");
+// var greeting = "Hello "
+// document.write("<h1>" + greeting + getUserName + "</h1>");
+
+
+// var userAge = prompt("how old are you?");
+// if (isNaN(userAge)) {
+//     console.log("age in alpha ", userAge)
+// } else {
+//     console.log("Your age is: ", userAge)
+// }
